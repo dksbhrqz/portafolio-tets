0e378960fd66ad083722fdbeee2c636d0244e293
Derkis Bohórquez <derkis.bohorquez@smartquick.com.co>
2021-08-27 13:01:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Portafolio", message: "Hello there!" });
+});
+
+app.use("/", router);
+
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
